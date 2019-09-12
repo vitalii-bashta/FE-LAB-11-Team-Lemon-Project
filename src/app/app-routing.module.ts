508f6e5c77812d0eb7cfd2954a2bc5f3ebcfbda4
Roman Saldan <romasaldan@gmail.com>
2019-09-12 13:07:24 +0300

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home-page/home-page.routing').then(m => m.ArtecleLoadModule)
+  },
+  { 
+    path: '**', 
+    redirectTo: 'home' 
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
