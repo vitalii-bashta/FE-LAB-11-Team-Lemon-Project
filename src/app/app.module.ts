40ea14bfdbe86f82d33a14a672152ac752d4a83a
Roman Saldan <romasaldan@gmail.com>
2019-09-12 20:23:53 +0300

    added service for firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { CoreModule } from 'src/app/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpService } from './services/http-service'
 
 @NgModule({
   declarations: [
@@ -11,10 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     CoreModule,
   ],
-  providers: [],
+  providers: [
+    HttpService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from './../../services/http-service'
+
+import { Event } from '../../core/models/event.model'
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-
-  constructor() { }
-
+  // :Observable<User[]>
+  public fakeData ={
+    body: "quia et suscipit\nsuscipit recusandae consequunt...",
+    id: 50,
+    title: "sunt aut facere repellat provident occaecati ex...",
+    userId: 1
+  }
+  constructor(private http: HttpService) { }
+  private courses$
   ngOnInit() {
+    this.http.getEvents().subscribe({
+      next:users => console.log(users)
+    })
+    this.http.deleteEvents('3').subscribe({
+      next:user => console.log(user)
+    })
+    this.http.getEvent('4').subscribe({
+      next:user => console.log(user)
+    })
+
+    // this.http.pushUser(this.fakeData).subscribe({
+    //   next:user => console.log(user)
+    // })
+    this.http.updateEvents('3',this.fakeData).subscribe({
+      next:user => console.log(user)
+    })
   }
 
 }
