c155ee0502e5d49717ab76f5fe6f3b349d793aad
AndriiPetrov1 <Bratok3000@gmail.com>
2019-09-23 18:51:22 +0300

    addingRouting

--- src/app/user-profile/user-profile.module.ts
+++ src/app/user-profile/user-profile.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
-
+import { UserProfileComponent } from '';
 
 @NgModule({
   declarations: [],
