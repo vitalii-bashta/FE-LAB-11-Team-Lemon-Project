508543df7cc24ca8f6b7d716b8f2ff1f68e61890
vitalii-bashta <bashtavitaliy@gmail.com>
2019-09-24 22:49:43 +0300

    added styles

--- src/app/home/home-page.routing.module.ts
+++ src/app/home/home-page.routing.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FooterComponent } from './components/footer/footer.component';
 import { HomePageComponent } from './home-page.component';
+import { HeaderComponent } from './components/header/header.component'
+import { FooterComponent } from './components/footer/footer.component';
+import { AdressesComponent } from './components/adresses/adresses.component';
+import { EventsComponent } from './components/events/events.component';
+import { OrganizationsComponent } from './components/organizations/organizations.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { VolunteersComponent } from './components/volunteers/volunteers.component';
 
-const routes : Routes = [
-  {
-    path: '',
-    component: HomePageComponent
-  },
-]
+const routes: Routes = [
+	{
+		path: '',
+		component: HomePageComponent,
+		children: [
+			{ path: '', redirectTo: 'events', pathMatch: 'full' },
+			{ path: 'events', component: EventsComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'volunteers', component: VolunteersComponent },
+      { path: 'organizations', component: OrganizationsComponent },
+      { path: 'adresses', component: AdressesComponent },
+		]
+	}
+];
 
 @NgModule({
   imports: [
