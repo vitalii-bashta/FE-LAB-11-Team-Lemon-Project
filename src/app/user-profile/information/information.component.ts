ddb407084a24532509efdce85093adc407b06379
AndriiPetrov1 <Bratok3000@gmail.com>
2019-10-02 19:17:55 +0300

    rebuild array to object

--- src/app/user-profile/information/information.component.ts
+++ src/app/user-profile/information/information.component.ts
@@ -7,13 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InformationComponent implements OnInit {
 
-  public information = [
-    '+38 (097) 123-4567',
-    "someEmail@pwc.com",
-    "Lviv",
-    "25 years old",
-    "SirPlus"
-  ]
+  information = {
+    'mobile': '+38 (097) 123-4567',
+    'email': 'someEmail@pwc.com',
+    'city': 'Lviv',
+    'age': '25 years old',
+    'memberOf': 'SirPlus'
+  }
+
+  // public information = [
+  //   '+38 (097) 123-4567',
+  //   'someEmail@pwc.com',
+  //   'Lviv',
+  //   "25 years old",
+  //   "SirPlus"
+  // ]
+  
   public skills = "Front-end developer, RPA developer";
   public aboutMe = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris a semper diam, vel laoreet arcu. Integer vestibulum, nibh vestibulum accumsan accumsan, ipsum elit luctus mauris, ac fermentum quam leo elementum quam. Fusce non elit porttitor, condimentum metus eget, facilisis lacus";
   
