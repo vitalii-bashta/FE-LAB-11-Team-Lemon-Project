697362bbbe624818f64672c8206918e3f7ccc8d4
Roman Saldan <romasaldan@gmail.com>
2019-10-05 19:43:07 +0300

    resolving conflict for http.service

--- src/app/core/services/http-events.service.ts
+++ src/app/core/services/http-events.service.ts
@@ -2,9 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { Event } from '../models/event.model'
-
-  
 @Injectable()
 export class HttpServiceEvents{
     private fireBase = `https://fe-lab-11-team-lemon-project.firebaseio.com/`
@@ -15,16 +12,16 @@ export class HttpServiceEvents{
     getEvents():Observable<any[]> {
         return this.http.get<any[]>(this.fireBase+`newevents.json`)
     }
-  getEvent(number:string):Observable<any> {
-    return this.http.get(`${this.fireBase}events/${number}.json`)
-  }
-  deleteEvent(id:string):Observable<void> {
-    return this.http.delete<void>(`${this.fireBase}events/${id}.json`)
-  }
-  pushEvent(data:Event):Observable<any>  {
-    return this.http.post(`${this.fireBase}events.json`, data)
-  }
-  updateEvent(key:string,data:Event):Observable<any> {
-    return this.http.patch(`${this.fireBase}/events/${key}.json`,data)
-  }   
+    getEvent(number:string):Observable<any> {
+        return this.http.get(`${this.fireBase}events/${number}.json`)
+    }
+    deleteEvent(id:string):Observable<void> {
+        return this.http.delete<void>(`${this.fireBase}events/${id}.json`)
+    }
+    pushEvent(data):Observable<any>  {
+        return this.http.post(`${this.fireBase}events.json`, data)
+    }
+    updateEvent(key:string,data:any):Observable<any> {
+        return this.http.patch(`${this.fireBase}/events/${key}.json`,data)
+    }   
 }
