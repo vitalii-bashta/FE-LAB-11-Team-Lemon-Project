3ee656068113c09a321b11c60690a9dafee593f4
Roman Saldan <35783096+romasaldan@users.noreply.github.com>
2019-10-08 00:03:42 +0300

    Update events.service.ts

--- src/app/home/services/events.service.ts
+++ src/app/home/services/events.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { HttpServiceEvents } from '../../core/services/http-events.service';
-// import { EventData } from '../models/index';
+import { EventData } from '../models/index';
 
 @Injectable({
   providedIn: 'root'
