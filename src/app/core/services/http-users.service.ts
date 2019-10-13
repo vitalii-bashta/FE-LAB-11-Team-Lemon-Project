48db02cb78db713c3eddae2980d52069ed35bd60
Roman Saldan <romasaldan@gmail.com>
2019-10-14 00:26:24 +0300

    updated get functions in http-client, added possibility to send filtered requests

--- src/app/core/services/http-users.service.ts
+++ src/app/core/services/http-users.service.ts
@@ -9,19 +9,23 @@ import { User } from '../models/user.model'
 export class HttpServiceUsers{
   private fireBase = `https://fe-lab-11-team-lemon-project.firebaseio.com/`
   constructor(private http: HttpClient){ }  
-  getUsers():Observable<User[]> {
-    return this.http.get<User[]>(this.fireBase+`users.json`)
+  getUsers(request?:string):Observable<User[]> {
+	if(request) {
+			return this.http.get<User[]>(this.fireBase+`users.json?`+request)
+		} else {
+			return this.http.get<User[]>(this.fireBase+`users.json`)
+		}
   }
-  getUser(number:string):Observable<any> {
-    return this.http.get(`${this.fireBase}users/${number}.json`)
+  getUser(key:string):Observable<any> {
+	return this.http.get(`${this.fireBase}users/${key}.json`)
   }
-  deleteUser(id:string):Observable<void> {
-    return this.http.delete<void>(`${this.fireBase}users/${id}.json`)
+  deleteUser(key:string):Observable<void> {
+	return this.http.delete<void>(`${this.fireBase}users/${key}.json`)
   }
-  pushUser(data:Event):Observable<any>  {
-    return this.http.post(`${this.fireBase}users.json`, data)
+  pushUser(data:User):Observable<any>  {
+	return this.http.post(`${this.fireBase}users.json`, data)
   }
-  updateUser(key:string,data:Event):Observable<any> {
-    return this.http.patch(`${this.fireBase}/users/${key}.json`,data)
+  updateUser(key:string,data:User):Observable<any> {
+	return this.http.patch(`${this.fireBase}/users/${key}.json`,data)
   }   
 }
