5bcb9cbfe1cbae32fb4eea66fb52719361c00b3f
Bratok3000 <Bratok3000@gmail.com>
2019-10-13 22:00:10 +0300

    change from , to ; in the userInterface

--- src/app/core/models/user.model.ts
+++ src/app/core/models/user.model.ts
@@ -1,14 +1,14 @@
 export interface User {
-    id?:number,
-    name?: string,
-    email: string,
-    avatarUrl: string,
-    mobile?: string,
-    city?: string,
-    age?: number,
-    memberOf?: string,
-    skills?: string,
-    aboutMe?: string,
-    organizations?: string,
-    feedback?: string
+    id?:number;
+    name?: string;
+    email: string;
+    avatarUrl: string;
+    mobile?: string;
+    city?: string;
+    age?: number;
+    memberOf?: string;
+    skills?: string;
+    aboutMe?: string;
+    organizations?: string;
+    feedback?: string;
 }
