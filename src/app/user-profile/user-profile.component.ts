48b315853fd760efba32e72085df25abb565383a
AndriiPetrov1 <Bratok3000@gmail.com>
2019-10-14 18:14:10 +0300

    just testing fetching observable

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { User } from '../core/models/user.model';
+import { HttpServiceUsers } from '../core/services/http-users.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-profile',
@@ -11,26 +13,31 @@ export class UserProfileComponent implements OnInit {
 
   isActive = true;
   public fullName = "Robin Smith";
-  userData : User;
+  userData;
   
 
   constructor(private route: ActivatedRoute,
-              private router: Router) { }
+              private router: Router,
+              private _userService: HttpServiceUsers) { }
 
   ngOnInit() {
+    this.userData = this._userService.getUsers().subscribe(usersData =>  console.log(usersData[0]));
+    console.log(this.userData);
   }
 
-  showInformation() {
-    this.router.navigate(['information'], {relativeTo: this.route});
-    this.isActive = false;
-  }
-  showOrganization() {
-    this.router.navigate(['organization'], {relativeTo: this.route});
-    this.isActive = false;
-  }
-  showFeedback() {
-    this.router.navigate(['feedback'], {relativeTo: this.route});
-    this.isActive = false;
-  }
+
+
+  // showInformation() {
+  //   this.router.navigate(['information'], {relativeTo: this.route});
+  //   this.isActive = false;
+  // }
+  // showOrganization() {
+  //   this.router.navigate(['organization'], {relativeTo: this.route});
+  //   this.isActive = false;
+  // }
+  // showFeedback() {
+  //   this.router.navigate(['feedback'], {relativeTo: this.route});
+  //   this.isActive = false;
+  // }
 
 }
